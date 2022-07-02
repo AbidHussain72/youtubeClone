49d3af869dc7a0770b3d7e8f43151e99e44c9828
AbidHussain72 <83560278+AbidHussain72@users.noreply.github.com>
2022-07-01 19:25:03 -0700

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+ob_start(); //Turns on output buffering 
+session_start();
+
+
+
+try {
+    $con = new PDO("mysql:dbname=VideoTube;host=localhost", "root", "");
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
